5ad274c9c4ed10cd6e65fd80d57a9a2b5cc0a2c5
JuanRendon56 <A01731524@itesm.mx>
2023-03-28 19:12:46 -0600

    Landing Page 1.0

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,77 +1,37 @@
 import * as React from 'react';
-import SmartToyIcon from '@mui/icons-material/SmartToy';
-import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
-import { Box, ToggleButtonGroup, ToggleButton, AppBar, Toolbar, Typography, createTheme, Button } from '@mui/material';
+import { Box, Typography, createTheme, Button, ThemeProvider } from '@mui/material';
 import { Container } from '@mui/system';
-import { Navigate } from 'react-router-dom';
 
 const theme = createTheme({
     palette: {
-      principal: {
-        main: '#64748B',
+      primary: {
+        main: '#212121',
         contrastText: '#fff',
       },
     },
-  });
+});
 
 function Landing() {
-    const [alignment, setAlignment] = React.useState('web');
-
-    const handleChange = (event, newAlignment) => {
-    setAlignment(newAlignment);
-    };
-
   return (
     <Box sx={{ flexGrow: 1 }}>
-        <AppBar color="secondary" position="static">
-            <Toolbar>
-                <SmartToyIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-                <Typography
-                    variant="h6"
-                    noWrap
-                    component="a"
-                    href="/"
-                    sx={{
-                    mr: 2,
-                    display: { xs: 'none', md: 'flex' },
-                    fontFamily: 'monospace',
-                    fontWeight: 700,
-                    letterSpacing: '.3rem',
-                    color: 'inherit',
-                    textDecoration: 'none',
-                    }}
-                >
-                    INTELLIGENTBOT
-                </Typography>
-                <ToggleButtonGroup
-                    color="primary"
-                    value={alignment}
-                    exclusive
-                    onChange={handleChange}
-                    aria-label="Platform"
-                    sx={{ marginLeft: "auto"}}
-                >
-                    <ToggleButton value="ES">ES</ToggleButton>
-                    <ToggleButton value="EN">EN</ToggleButton>
-                </ToggleButtonGroup>   
-            </Toolbar>
-        </AppBar>
-        <Typography variant='h2' gutterBottom align='center'>
+        <Typography variant='h2' gutterBottom align='center' sx={{marginTop: 5, marginBottom: 5}}>
             ¡Bienvenido a tu IntelligentBot!
         </Typography>
         <Container align='center'>
-        <QuestionAnswerIcon sx={{ fontSize: 350}}></QuestionAnswerIcon>
+        <Box component="img" src="/BOT.jpg"/>        
         </Container>
-        <Typography variant='h6' gutterBottom align='center'>
+        <Typography variant='h6' gutterBottom align='center' paragraph='true' sx={{marginLeft: 65, marginRight: 65, marginBottom: 5, marginTop: 3}}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </Typography>
         <Container align='center'>
-        <Button variant='contained' color='primary' onClick={event => window.location.href='/login'}>
-            LogIn
-        </Button>
-        <Button variant='contained' color='primary' sx={{ marginLeft: 4}} onClick={event => window.location.href='/signup'}>
-            SignUp
-        </Button>
+            <ThemeProvider theme={theme}>
+                <Button variant='contained' onClick={event => window.location.href='/login'}>
+                    LogIn
+                </Button>
+                <Button variant='contained' color='primary' sx={{ marginLeft: 4}} onClick={event => window.location.href='/signup'}>
+                    SignUp
+                </Button>
+            </ThemeProvider>
         </Container>
     </Box>
   );
